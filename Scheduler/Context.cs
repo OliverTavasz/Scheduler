f70288270a96fe84f83a77d6ce4af81d228619ba
- <->
2025-10-10 12:00:45 +0200

    Moved Context into its own file

---
+++ Scheduler/Context.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Scheduler
+{
+    public class Context(DbContextOptions<Context> options) : DbContext(options)
+    {
+        public DbSet<Session> Sessions { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Schedule>()
+        //        .HasMany(s => s.Sessions);
+        //}
+        public void ConstructSchedules()
+        {
+            ScheduleManager.FullSchedule = [];
+
+            //for(int i = 0; i < Sessions.Count(); i++)
+            //{
+
+            //}
+
+            foreach(Session s in Sessions)
+            {
+                if (!DateOnly.TryParse(s.date, out DateOnly d))
+                {
+                    Sessions.Remove(s);
+                    continue;
+                }
+
+                if (!ScheduleManager.FullSchedule.TryGetValue(d, out Schedule? sd))
+                {
+                    sd = new Schedule(d);
+                    ScheduleManager.FullSchedule.Add(d, sd);
+                }
+
+                if (!sd.AddSessions([s]))
+                {
+                    Sessions.Remove(s);
+                    continue;
+                }
+            }
+        }
+        public bool AddSessions(Session[] session)
+        {
+            for (int i = 0; i < session.Length; i++)
+            {
+                if (Sessions.Contains(session[i]))
+                    continue;
+                Sessions.Add(session[i]);
+            }
+            return true;
+        }
+        public bool RemoveSession(Session session)
+        {
+            if (Sessions.Contains(session))
+            {
+                Sessions.Remove(session);
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- Scheduler/ScheduleManager.cs
+++ Scheduler/ScheduleManager.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -59,66 +58,6 @@ namespace Scheduler
         {
             FullSchedule.TryGetValue(date, out Schedule? s);
             return s;
-        }
-    }
-
-    public class Context(DbContextOptions<Context> options) : DbContext(options)
-    {
-        public DbSet<Session> Sessions { get; set; }
-
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<Schedule>()
-        //        .HasMany(s => s.Sessions);
-        //}
-        public void ConstructSchedules()
-        {
-            ScheduleManager.FullSchedule = [];
-
-            //for(int i = 0; i < Sessions.Count(); i++)
-            //{
-
-            //}
-
-            foreach(Session s in Sessions)
-            {
-                if (!DateOnly.TryParse(s.date, out DateOnly d))
-                {
-                    Sessions.Remove(s);
-                    continue;
-                }
-
-                if (!ScheduleManager.FullSchedule.TryGetValue(d, out Schedule? sd))
-                {
-                    sd = new Schedule(d);
-                    ScheduleManager.FullSchedule.Add(d, sd);
-                }
-
-                if (!sd.AddSessions([s]))
-                {
-                    Sessions.Remove(s);
-                    continue;
-                }
-            }
-        }
-        public bool AddSessions(Session[] session)
-        {
-            for (int i = 0; i < session.Length; i++)
-            {
-                if (Sessions.Contains(session[i]))
-                    continue;
-                Sessions.Add(session[i]);
-            }
-            return true;
-        }
-        public bool RemoveSession(Session session)
-        {
-            if (Sessions.Contains(session))
-            {
-                Sessions.Remove(session);
-                return true;
-            }
-            return false;
         }
     }
 }
